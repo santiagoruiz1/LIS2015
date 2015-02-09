8f4b8941d08ee0c2fd3f330500fd718e615b1e7e
santiago.ruiz1 <santiago.ruiz1@localhost>
2015-02-09 14:47:16 -0500

    Version Inicial

---
+++ src/java/Persona.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author santiago.ruiz1
+ */
+public class Persona {
+    private String nombre;
+    private int edad;
+    
+}
